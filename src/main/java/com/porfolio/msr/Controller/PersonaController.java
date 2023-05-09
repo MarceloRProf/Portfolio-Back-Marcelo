5076130f29b7857df537fe1f1b54d266176e0094
MarceloRProf <marcelosriss@gmail.com>
2023-05-09 19:01:08 -0300

    actualizacionfinal

--- src/main/java/com/porfolio/msr/Controller/PersonaController.java
+++ src/main/java/com/porfolio/msr/Controller/PersonaController.java
@@ -16,10 +16,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
+
 @RequestMapping("/personas")
 public class PersonaController {
     
