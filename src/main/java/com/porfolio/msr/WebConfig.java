b1f0bfb257bdcb92ad3443125dbfe557c18f111d
MarceloRProf <marcelosriss@gmail.com>
2023-05-09 23:09:31 -0300

    deployRenderTres

--- src/main/java/com/porfolio/msr/WebConfig.java
+++ src/main/java/com/porfolio/msr/WebConfig.java
@@ -9,7 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*");
+        
     }
             
 }
